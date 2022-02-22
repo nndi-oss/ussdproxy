995dae6805dfd4cffb8ac8aeacbb19d45507cebb
Zikani Nyirenda Mwase <zikani.nmwase@ymail.com>
2022-02-22 23:49:31 +0200

    fix: Influx app can now send ilp data proper
    Tested with QuestDB's ILP support though that caused a few headaches because of quest's data types :facepalm:

--- app/influx/influx.go
+++ app/influx/influx.go
@@ -2,6 +2,7 @@ package influx
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"strconv"
 	"strings"
@@ -33,7 +34,7 @@ type InfluxDbApp struct {
 func NewInfluxApp(addr, database, user, password string) *InfluxDbApp {
 	conn1, err := net.Dial("tcp", addr) // test the conneciotn
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("failed to connect to %s got: %v ", addr, err))
 	}
 	defer conn1.Close()
 
@@ -54,57 +55,74 @@ func (app *InfluxDbApp) UseSession(session ussdproxy.Session) {
 	app.session = session
 }
 
-func (app *InfluxDbApp) onDataWriteToInflux(data []byte) error {
-	tags := map[string]string{
-		"app": "ussdproxy:influxdb",
-	}
-	fields := map[string]interface{}{
-		"timestamp": time.Now().Unix(),
-	}
-	// TODO: Parse the tags and fields
-	if app.InputFormat == FormatPipeDelimited {
-		// e.g. temp:38.29|lat:-35.012|lng:12.12|tag_meter_no:ABCD|ts:145923830|tag_host:2981
-		// for each string starting with tag use as tag,
-		// use everything else as a field
-		for _, entry := range strings.Split(string(data), "|") {
-			keyValueItem := strings.Split(entry, ":")
-			if len(keyValueItem) == 2 {
-				keyPart := keyValueItem[0]
-				valuePart := keyValueItem[1]
+func extractPipeDelimitedTagsAndFields(data []byte) (map[string]string, map[string]interface{}) {
+	tags := make(map[string]string)
+	fields := make(map[string]interface{})
+	// e.g. temp:38.29|lat:-35.012|lng:12.12|tag_meter_no:ABCD|ts:145923830|tag_host:2981
+	// for each string starting with tag use as tag,
+	// use everything else as a field
+	for _, entry := range strings.Split(string(data), "|") {
+		keyValueItem := strings.Split(entry, ":")
+		if len(keyValueItem) == 2 {
+			// TODO(zikani03): fix where the __NODATA__ is coming from
+			keyPart := strings.ReplaceAll(keyValueItem[0], "__NODATA__", "")
+			valuePart := keyValueItem[1]
 
-				if keyPart == "ts" {
-					if timestampValue, err := strconv.ParseInt(valuePart, 10, 64); err == nil {
-						fields["timestamp"] = timestampValue
-					}
+			if keyPart == "ts" {
+				if timestampValue, err := strconv.ParseInt(valuePart, 10, 64); err == nil {
+					fields["ts"] = timestampValue
 				}
+			}
 
-				if intValue, err := strconv.ParseInt(valuePart, 10, 64); err == nil {
-					fields[keyPart] = intValue
-				} else if floatValue, err := strconv.ParseFloat(valuePart, 64); err == nil {
-					fields[keyPart] = floatValue
-				} else {
-					// not an integer or float so we just assume it's a tag now
-					tags[keyPart] = valuePart
-				}
+			if intValue, err := strconv.ParseInt(valuePart, 10, 64); err == nil {
+				fields[keyPart] = intValue
+			} else if floatValue, err := strconv.ParseFloat(valuePart, 64); err == nil {
+				fields[keyPart] = floatValue
+			} else {
+				// not an integer or float so we just assume it's a tag now
+				tags[keyPart] = valuePart
 			}
 		}
 	}
 
+	return tags, fields
+}
+
+func (app *InfluxDbApp) onDataWriteToInflux(data []byte) error {
+	var tags map[string]string
+	var fields map[string]interface{}
+
+	if app.InputFormat == FormatPipeDelimited {
+		tags, fields = extractPipeDelimitedTagsAndFields(data)
+		fields["ts"] = time.Now().UnixMilli()
+		tags["app"] = "ussdproxy"
+	}
+
 	conn, err := net.Dial("tcp", app.Addr)
 	if err != nil {
+		log.Fatalf("failed to connect %v", err)
 		return err
 	}
-	defer conn.Close()
 	serializer := lineprotocol.NewEncoder(conn)
 	serializer.SetMaxLineBytes(1024)
 	serializer.SetFieldTypeSupport(lineprotocol.UintSupport)
+	serializer.FailOnFieldErr(true)
+	// serializer.SetPrecision(time.Millisecond)
 
 	event, err := lineprotocol.New(app.Database, tags, fields, time.Now())
 	if err != nil {
 		return err
 	}
-	serializer.Encode(event)
-	return err
+
+	_, err = serializer.Encode(event)
+	if err != nil {
+		return fmt.Errorf("failed to send data got: %v", err)
+	}
+	err = conn.Close()
+	if err != nil {
+		return fmt.Errorf("failed to close connection got %v", err)
+	}
+	return nil
 }
 
 // Name the name of the application
@@ -135,10 +153,10 @@ func (app *InfluxDbApp) OnError(request ussdproxy.UdcpRequest, session ussdproxy
 
 // OnData returns the request/response handler for the Echo Application
 func (app *InfluxDbApp) OnData(request ussdproxy.UdcpRequest, session ussdproxy.Session) (ussdproxy.UdcpResponse, error) {
-	if request.HasMoreToSend() {
-		// Waiting for Client to send more data
-		return ussdproxy.NewReceiveReadyResponse(), nil
-	}
+	// if request.HasMoreToSend() {
+	// 	// Waiting for Client to send more data
+	// 	return ussdproxy.NewReceiveReadyResponse(), nil
+	// }
 	// Handle the decoding of the data here
 	//
 	// This is the point at which you may send data to an external service
--- cmd/influx_app.go
+++ cmd/influx_app.go
@@ -13,9 +13,8 @@ var influxAppCmd = &cobra.Command{
 	Short: "Starts the influx proxy server",
 	Long:  `Starts the influx proxy server`,
 	Run: func(cmd *cobra.Command, args []string) {
-		addr := "localhost:3000"
-		influxApplication := influx.NewInfluxApp(addr, "test_database", "user", "password")
-		if err := server.ListenAndServe(addr, influxApplication); err != nil {
+		influxApplication := influx.NewInfluxApp("127.0.0.1:9009", "ussdproxy", "user", "password")
+		if err := server.ListenAndServe("localhost:3000", influxApplication); err != nil {
 			log.Fatalf("Failed to start Influx Application. Error %s", err)
 		}
 	},
